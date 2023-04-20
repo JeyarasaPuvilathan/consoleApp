d3fbc9e8417102fec5b01779b60909f70e75fe3b
JeyarasaPuvilathan <jp21puvi@gmail.com>
2023-04-20 15:09:09 +0800

    Added Console Application

---
+++ NCSConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NCSConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("window Size:" + 3);
+
+SimpleMovingAverage simpleMovingAverage = new SimpleMovingAverage(3);
+simpleMovingAverage.Submit(2);
+simpleMovingAverage.Submit(5);
+
+double result = simpleMovingAverage.ComputeAverage();
+
+Console.WriteLine("Average is :" + result);
+
+simpleMovingAverage.Submit(4);
+
+double result2 = simpleMovingAverage.ComputeAverage();
+
+Console.WriteLine("Average is :" + result2);
+
+simpleMovingAverage.Submit(3);
+
+double result3 = simpleMovingAverage.ComputeAverage();
+
+Console.WriteLine("Average is :" + result3);
+
+simpleMovingAverage.Submit(3);
+
+double result4 = simpleMovingAverage.ComputeAverage();
+
+Console.WriteLine("Average is :" + result4);
+
---
+++ NCSConsoleApp/SimpleMovingAverage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSConsoleApp
+{
+    public class SimpleMovingAverage
+    {
+        public readonly int windowSize;
+        List<int> storeWindow;
+        public SimpleMovingAverage( int windowSize) { 
+            this.windowSize = windowSize;
+            storeWindow = new List<int>();
+
+        }
+
+        public void Submit( int value)
+        {
+            storeWindow.Add(value);
+        } 
+
+        public double ComputeAverage()
+        {
+            double average = 0;
+            double total = 0;
+
+
+
+            for (int i = 0; i < this.windowSize; i++)
+            {
+
+                if (storeWindow.Count > this.windowSize)
+                {
+                    total += storeWindow[(storeWindow.Count -1) - i];
+                }
+               
+            }
+
+            if (storeWindow.Count > this.windowSize)
+            {
+                average = (double)total / (double)this.windowSize;
+                }
+            else
+            {
+                var hh = storeWindow.Sum();
+                average = (double)storeWindow.Sum() / (double)storeWindow.Count;
+            }
+
+            return average;
+        }
+    }
+}
